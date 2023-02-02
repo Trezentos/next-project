d8e0e61fef7f52fd7e6e1ba34b7686daa6568f10
fagundes <gustavofagundes1998@hotmail.com>
2023-02-02 20:10:12 -0300

    last updats

---
+++ src/pages/success.tsx
@@ -0,0 +1,77 @@
+import { stripe } from "@/lib/stripe";
+import { ImageContainer, SuccessContainer } from "@/styles/pages/success";
+import { GetServerSideProps } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import Link from "next/link";
+import Stripe from "stripe";
+
+interface SuccessProps {
+    customerName: string;
+    product: {
+        name: string;
+        imageUrl: string;
+    }
+}
+
+export default function Success({ customerName, product }: SuccessProps) {
+    return (
+        <>
+        <Head>
+            <title>Compra efetuada | Ignite Shop</title>
+            <meta name="robots" content="noindex" />
+        </Head>
+
+        <SuccessContainer>
+            <h1>Compra Efetuada!</h1>
+
+
+            <ImageContainer>
+                <Image alt="" src={product.imageUrl} width={160}  height={160}/> 
+            </ImageContainer>
+
+            <p>
+                Parabéns <strong>{customerName}</strong>, você adquiriu esta nova camiseta: <strong>{product.name}</strong> 😆
+            </p>
+
+            <Link href="/" >
+                Voltar ao catálogo
+            </Link>
+        </SuccessContainer >
+        </>
+    )
+}
+
+export const getServerSideProps: GetServerSideProps = async ({ query }) => {
+
+    if (!query.session_id) {
+        return {
+            redirect: {
+                destination: '/',
+                permanent: false,
+            }
+        }
+    }
+
+
+    const sessionId = String(query.session_id)
+
+   
+
+    const session = await stripe.checkout.sessions.retrieve(sessionId, {
+        expand: ['line_items', 'line_items.data.price.product']
+    })
+
+    const customerName = session.customer_details?.name
+    const product = session.line_items?.data[0].price?.product as Stripe.Product;
+
+    return {
+        props: {
+            customerName,
+            product: {
+                name: product.name,
+                imageUrl: product.images[0]
+            }
+        }
+    }
+}
